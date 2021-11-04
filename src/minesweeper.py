da4b308671c707bc801a904e2be8c31ba73ac338
Richard Norling <rich.norling@gmail.com>
2021-11-04 22:58:06 +0100

    Add arcade library and start a GUI from main thread

--- src/minesweeper.py
+++ src/minesweeper.py
@@ -1,22 +1,67 @@
 #!/usr/bin/env python3
 
-import random
-import numpy as np
+"""
+Minesweeper Game
+"""
+
+import arcade
 
 from enums.difficulties import Difficulty 
 from boards import MineBoard, PlayerBoard
 
-# Board definitions
+# Constants
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 650
+SCREEN_TITLE = "Minesweeper"
+
+# Pyglet Constants
+ANTIALIASING=False
+
+# Board Constants
 BOARD_DIFFICULTY = Difficulty.MEDIUM
 BOARD_WIDTH = 10
 BOARD_HEIGHT = 10
 
-def main():
-    mine_board = MineBoard(BOARD_WIDTH,BOARD_HEIGHT,BOARD_DIFFICULTY)
-    mine_board.print_board()
+class Minesweeper(arcade.Window):
+    """
+    Main application class.
+    """
 
-    player_board = PlayerBoard(BOARD_WIDTH,BOARD_HEIGHT)
-    player_board.print_board()
+    def __init__(self):
+
+        # Call the parent class and set up the window
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE, antialiasing=ANTIALIASING)
+
+        arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
+
+    def setup(self):
+        """Set up the game here. Call this function to restart the game."""
+        mine_board = MineBoard(BOARD_WIDTH,BOARD_HEIGHT,BOARD_DIFFICULTY)
+        mine_board.print_board()
+
+        player_board = PlayerBoard(BOARD_WIDTH,BOARD_HEIGHT)
+        player_board.print_board()
+        pass
+
+    def on_draw(self):
+        """Render the screen."""
+
+        arcade.start_render()
+        # Code to draw the screen goes here
+
+def main():
+    """Main function"""
+    window = Minesweeper()
+    window.setup()
+    arcade.run()
+
+    #import pyglet
+    #pyglet.window.Window()
+
+    #from pyglet.window import Window
+    #from pyglet.gl import Config;
+    #w = Window(config=Config(major_version=4, minor_version=1))
+    #print('{}.{}'.format(w.context.config.major_version, w.context.config.minor_version))
 
 if __name__ == "__main__":
     main()
